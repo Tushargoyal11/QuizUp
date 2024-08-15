1e2ef0e9ef6af4fbd3dc6777c1dd6795ecfbc83c
Tushargoyal11 <tgoyal2001@gmail.com>
2024-08-15 06:28:33 +0530

    connect login and splash screen (so that after 3000 milliseconds of splash screen user reaches sign up screen) in MainActivity.kt
    Designed Login screen (hard coded the text in it)

--- app/src/main/java/com/example/quizup/Login.kt
+++ app/src/main/java/com/example/quizup/Login.kt
@@ -1,28 +1,16 @@
 package com.example.quizup
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.postDelayed
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        Handler(Looper.getMainLooper()).postDelayed(3000){
-
-            val intent=Intent(this, Login::class.java)
-            startActivity(intent)
-
-        }
-
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
